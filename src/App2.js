44e545c0130045c764bee24f83ac375755bb1dce
FuryNashwaAspirna <FurryAspirnaa11@gmail.com>
2019-12-10 14:50:59 +0700

    praktek-uas

---
+++ src/App2.js
@@ -0,0 +1,25 @@
+import React, { Component} from 'react'
+import './App2.css'
+
+function App() {
+    return (
+        <div className="wrapper gambar ">
+            <div className="flex-wrapper">
+                <div className="flex-judul">
+                    <h1>Save your Earth</h1>
+                    <div className="deskripsi"> Menanam satu Pohon, menghasilkan satu juta manfaat</div>
+                </div>
+            </div>
+     <div className="line"> 
+        <div className="text"> Hubungi Kami </div>
+     </div>
+     <div className="line1"> 
+        <div className="text1"> Donasi Pohon</div>
+     </div>
+        </div>
+        
+     );
+   }
+   
+   export default App;
+    
